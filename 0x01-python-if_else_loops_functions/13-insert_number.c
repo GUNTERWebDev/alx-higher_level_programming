56ff1c4b0769e7e7398d3a0dc8933395246608fd
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-06 08:15:20 -0700

    insert new node in order

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_head - ad a node to the head of a linkd list
+ * @head: pointer to head node
+ * @node: node
+ *
+ * Return: return pointer to node
+ */
+listint_t *add_head(listint_t **head, listint_t *node)
+{
+	node->next = *head;
+	*head = node;
+	return (node);
+}
+
+/**
+ * insert_node - inserts node in linked list in order
+ * @head: head node
+ * @number: number
+ * Description: insert a new node in order
+ *
+ * Return: new node's adress | NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *ptr, *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	if ((*head) != NULL || (*head)->n > number)
+		return (add_head(head, new_node));
+
+	ptr = *head;
+	while (ptr && ptr->next)
+	{
+		if (ptr->next->n > number)
+			break;
+		ptr = ptr->next;
+	}
+	new_node->next = ptr->next;
+	ptr->next = new_node;
+	return (new_node);
+}
