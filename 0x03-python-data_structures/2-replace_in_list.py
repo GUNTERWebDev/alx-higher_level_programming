677c8858fa2c09646e3c6a221e94967eae350ee0
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-09 04:57:01 -0700

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from 2-main import *
+
+
+def replace_in_list(my_list, idx, element):
+    count = 0
+    for i in my_list:
+        if count == idx:
+            if my_list[idx] < 0:
+                return (my_list)
+            else:
+                my_list[idx] = element
+        count += 1
+    if count > idx:
+        return (my_list)
