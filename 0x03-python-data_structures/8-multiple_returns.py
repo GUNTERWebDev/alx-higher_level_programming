6389b4b340168e95be2ae7fe39399782fdc92c40
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-09 07:24:41 -0700

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ from 8-main import *
 
 
 def multiple_returns(sentence):
-    if sentence != '\0':
+    if len(sentence) != 0:
         new_tuple = (len(sentence), sentence[0])
         return (new_tuple)
     else:
