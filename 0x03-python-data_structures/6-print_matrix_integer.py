062f2d59014c1354b1ca87450cf5ad6cb8ef61b2
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-10 16:57:25 -0700

    matrix is comming for u

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        count = 0
+        count = 1
         for j in i:
-            if len(i) == count:
+            if count == len(i):
                 print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
