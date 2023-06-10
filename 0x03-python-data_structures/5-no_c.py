d706d84e1af3b5ed6d832e9ede0e53a129f6dd90
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-10 16:32:00 -0700

    delete C and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    first_string = my_string.translate({ord('c'): None}
-    new_string = first_string.translate({ord('C'): None}
+    first_string = my_string.translate({ord('c'): None})
+    new_string = first_string.translate({ord('C'): None})
     return (new_string)
